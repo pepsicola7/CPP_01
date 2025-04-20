e0be8ddc5a0f2ddb9094e9281a832b2cbfbdd437
Prprkitty <168284039+pepsicola7@users.noreply.github.com>
2025-04-20 13:23:33 +0200

    commit the first

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 17:10:53 by peli              #+#    #+#             */
+/*   Updated: 2025/04/18 17:44:22 by peli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class	Zombie
+{
+	public:
+	void	announce( void );
+
+	private:
+	std::string	name;
+};
+
+#endif
