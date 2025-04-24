3fa20526079fa9829bf12f1adfaa38cf5d136aa2
Prprkitty <168284039+pepsicola7@users.noreply.github.com>
2025-04-24 16:31:58 +0200

    add ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:13:19 by peli              #+#    #+#             */
-/*   Updated: 2025/04/23 20:04:16 by peli             ###   ########.fr       */
+/*   Updated: 2025/04/24 16:30:49 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int	parser_filename(const char* filename)
 {
@@ -22,8 +23,21 @@ int	parser_filename(const char* filename)
 	return (1);
 }
 
+std::string replace_all(std::string line, std::string s1, std::string s2)
+{
+	while (line.find(s1) != std::string::npos)
+	{
+		line = line.replace(line.find(s1), s1.length(), s2);
+	}
+	return (line);
+}
+
 int	main(int argc, char **argv)
 {
+	std::string	s1;
+	std::string	s2;
+	std::string	filename;
+
 	if (argc != 4)
 	{
 		std::cout << "Lack of the argument" << std::endl;
@@ -34,7 +48,34 @@ int	main(int argc, char **argv)
 		std::cout << "Error of the filename" << std::endl;
 		return (1);
 	}
-	
+	filename = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	if (s1.empty())
+	{
+		std::cerr << "ERROR : S1 CANNOT BE EMPTY" << std::endl;
+		return (1);
+	}
+	std::ifstream	input_file(filename.c_str());
+	if (!input_file)
+	{
+		std::cerr << "Error : fail to open the file" << std::endl;
+		return (1);
+	}
+	std::string out_filename = std::string(filename) + ".replace";
+	std::ofstream	output_file(out_filename.c_str());
+	if (!output_file)
+	{
+		std::cerr << "Error : fail to copy the file" << std::endl;
+		return (1);
+	}
+	std::string	line;
+	while (std::getline(input_file, line))
+	{
+		std::string replace_line = replace_all(line, s1, s2);
+		output_file << replace_line << std::endl;
+	}
+	input_file.close();
+	output_file.close();
 	return (0);
 }
-
