618fae11f36c85f736d16ea92cce12e288c8bb77
Peiqi <perrine0624@gmail.com>
2025-04-25 18:27:49 +0200

    add ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,18 @@
+
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class	Harl
+{
+private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+public:
+	void complain( std::string level );;
+};
+
+
---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+
+#include "Harl.hpp"
+
+int	main()
+{
+	Harl	Harl;
+	Harl.complain("Unknow level");
+	Harl.complain("DEBUG");
+	Harl.complain("INFO");
+	Harl.complain("WARNING");
+	Harl.complain("ERROR");
+}
